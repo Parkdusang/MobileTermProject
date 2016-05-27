482786afb3d5ea99f130bb18e7c1278fe2d20726
parkdusang <pesang72@hanmail.net>
2016-05-28 01:33:27 +0900

    progress잔버그수정

--- HealthTrainer/app/src/main/java/com/example/parkdusang/healthtrainer/Cc1Diaglog.java
+++ HealthTrainer/app/src/main/java/com/example/parkdusang/healthtrainer/Cc1Diaglog.java
@@ -4,7 +4,6 @@ import android.app.Dialog;
 import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,34 +12,57 @@ import android.widget.EditText;
  * Created by yooyj on 2016-05-05.
  */
 
-public class Cc1Diaglog extends Dialog implements View.OnTouchListener {
-    EditText startweight, goalweight, currentweight;
+public class Cc1Diaglog extends Dialog {
+    EditText  goalweight, currentweight;
     private Button dialog_btn1, dialog_btn2;
     private String sw, gw, cw;
-
+    private String edtx1,edtx2;
+    boolean check = true;
     public Cc1Diaglog(Context context) {
         super(context);
+        Log.i("TAG", "Cc1Diaglog: ");
     }
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.cc1dialog);
 
-        startweight = (EditText) findViewById(R.id.startweight);
+        Log.i("TAG", "onCreate: ");
         goalweight = (EditText) findViewById(R.id.goalweight);
         currentweight = (EditText) findViewById(R.id.currentweight);
 
         dialog_btn1 = (Button) findViewById(R.id.dialog_btn1);
         dialog_btn2 = (Button) findViewById(R.id.dialog_btn2);
 
-        dialog_btn1.setOnTouchListener(this);
-        dialog_btn2.setOnTouchListener(this);
+        dialog_btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                edtx1 = goalweight.getText().toString();
+                edtx2 = currentweight.getText().toString();
+                check = true;
+                dismiss();
+            }
+        });
+        dialog_btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                check = false;
+                cancel();
+            }
+        });
     }
-
+    public String getedtxString1() {
+        return edtx1; // goal
+    }
+    public String getedtxString2() {
+        return edtx2; // current
+    }
     public String getStartWeight() {
         return sw;
     }
-
+    public void setStartWeight(String sw) {
+        this.sw =  sw;
+    }
     public String getGoalWeight() {
         return gw;
     }
@@ -48,21 +70,15 @@ public class Cc1Diaglog extends Dialog implements View.OnTouchListener {
     public String getCurrentWeight() {
         return cw;
     }
-
-    @Override
-    public boolean onTouch(View v, MotionEvent event) {
-        if (v.getId() == dialog_btn1.getId()) {
-            sw = startweight.getText().toString();
-            gw = goalweight.getText().toString();
-            cw = currentweight.getText().toString();
-
-            dismiss();
-        } else if (v.getId() == dialog_btn2.getId()) {
-            if (startweight.getText().toString().equals("") || goalweight.getText().toString().equals("") || currentweight.getText().toString().equals(""))
-                Log.e("dddd", "dfd");
-            else
-                cancel();
-        }
-        return false;
+    public void setGoalWeight(String gw) {
+        this.gw = gw;
     }
+
+    public void setCurrentWeight(String cw) {
+       this.cw = cw;
+    }
+    public boolean checkcancel(){
+        return check;
+    }
+
 }
--- HealthTrainer/app/src/main/java/com/example/parkdusang/healthtrainer/Customercontent1.java
+++ HealthTrainer/app/src/main/java/com/example/parkdusang/healthtrainer/Customercontent1.java
@@ -12,6 +12,7 @@ import android.widget.Button;
 import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
@@ -36,11 +37,11 @@ public class Customercontent1 extends Fragment {
     Button cc1btn;
     ArrayList<MyCustomDTO2> cc1list;
     ProgressBar cc1pBar;
-    TextView cc1txt1, cc1txt2 , oneword;
+    TextView cc1txt1, cc1txt2, oneword;
     Cc1Adapter cc1adapter;
     Cc1Diaglog cc1Dialog;
     String crtWeight, stWeight, goalWeight;
-    String id,myJSON;
+    String id, myJSON;
     InputStream is = null;
     String result = null;
     String line = null;
@@ -50,104 +51,105 @@ public class Customercontent1 extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         ViewGroup v = (ViewGroup) inflater.inflate(R.layout.customercontent1, container, false);
-        
+
         cc1listView = (ListView) v.findViewById(R.id.cc1listView);
-        
+
         cc1txt1 = (TextView) v.findViewById(R.id.cc1txt1);
         cc1txt2 = (TextView) v.findViewById(R.id.cc1txt2);
         oneword = (TextView) v.findViewById(R.id.trainr_oneword);
 
         id = this.getArguments().getString("_id", "None");
-        
-        
+
+
         cc1pBar = (ProgressBar) v.findViewById(R.id.cc1pBar);
-        
+
         cc1btn = (Button) v.findViewById(R.id.cc1btn);
 
 
         cc1Dialog = new Cc1Diaglog(getActivity());
-        cc1Dialog.setTitle("몸무게를 입력해주세요!");
+        cc1Dialog.setTitle("목표를 입력해주세요!");
         //다이어로그를 열어서 입력한 값을 얻어오는곳
         //이것도 디비에 저장해둬야 할듯
         //아무튼 앱을 껏다가 다시켜도 다시 이 몸무게 정보가 보일 수 있도록
         //현재 몸무게는 계속 수정할 수 있도록
-        
+
         cc1Dialog.setOnDismissListener(new DialogInterface.OnDismissListener() {
             @Override
             public void onDismiss(DialogInterface dialog) {
-                
-                stWeight = cc1Dialog.getStartWeight();
-                goalWeight = cc1Dialog.getGoalWeight();
-                crtWeight = cc1Dialog.getCurrentWeight();
-                
-                if (!(crtWeight.equals("") || stWeight.equals("") || goalWeight.equals(""))) {
-                    if (goalRate(crtWeight, stWeight, goalWeight) == 1) {
-                        cc1txt1.setText(cc1Dialog.getStartWeight() + "kg");
-                        cc1txt2.setText(cc1Dialog.getGoalWeight() + "kg");
-                        cc1btn.setText("범위를 넘어섰어요 ㅠㅠ");
-                        cc1btn.setText("현재 몸무게" + crtWeight);
-                        cc1pBar.setProgress(0);
-                    } else if (goalRate(crtWeight, stWeight, goalWeight) == 2) {
-                        cc1Dialog.startweight.setText("");
-                        cc1Dialog.goalweight.setText("");
-                        cc1Dialog.currentweight.setText("");
+                if (cc1Dialog.checkcancel()) {
+                    goalWeight = cc1Dialog.getedtxString1();
+                    crtWeight = cc1Dialog.getedtxString2();
+                    
+                    if (goalWeight == null) {
+                        goalWeight = "";
+                    }
+                    if (crtWeight == null) {
+                        crtWeight = "";
+                    }
+                    if (goalWeight.equals("") || crtWeight.equals("")) {
+                        Toast.makeText(getActivity(), "입력이 잘못되었습니다.", Toast.LENGTH_SHORT).show();
                     } else {
-                        cc1txt1.setText(cc1Dialog.getStartWeight() + "kg");
-                        cc1txt2.setText(cc1Dialog.getGoalWeight() + "kg");
-                        cc1btn.setText("현재 몸무게" + crtWeight);
-                        cc1pBar.setProgress((int) goalRate(crtWeight, stWeight, goalWeight));
+                        cc1Dialog.setGoalWeight(goalWeight);
+                        cc1Dialog.setCurrentWeight(crtWeight);
+                        cc1txt2.setText(goalWeight);
+
+                        cc1pBar.setProgress((int) goalRate(crtWeight, cc1Dialog.getStartWeight(), goalWeight));
+
+
                     }
                 }
             }
         });
-        
+
         cc1Dialog.setOnCancelListener(new DialogInterface.OnCancelListener() {
             @Override
             public void onCancel(DialogInterface dialog) {
-                cc1btn.setText("취소하셨어요 다시 입력하시겠어요?");
-                cc1txt1.setText("");
-                cc1txt2.setText("");
+                cc1btn.setText("test 설정하시겠어요?");
+
             }
         });
-        
+
         cc1btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //cc1Dialog.show();
+                cc1Dialog.show();
             }
         });
-        
+
         cc1list = new ArrayList<MyCustomDTO2>();
         cc1adapter = new Cc1Adapter(getActivity(), R.layout.list_row_exercise, cc1list);
-        
+
         cc1listView.setAdapter(cc1adapter);
 
-        getData(url ,id);
+        getData(url, id);
         return v;
     }
-    
+
     private float goalRate(String c, String s, String g) {
         float cw, sw, gw;
         float result = 0;
-        if (c.equals("") || s.equals("") || g.equals(""))
-            result = 2;
-        else {
-            cw = Float.parseFloat(c);
-            sw = Float.parseFloat(s);
-            gw = Float.parseFloat(g);
-            
-            if (sw < gw) {
-                if (cw <= gw && cw >= sw) {
-                    result = ((cw - sw) / (gw - sw)) * 100;
-                } else
-                    result = 1; //범위 밖에 있을 때
-            } else if (sw > gw) {
-                if (cw >= gw && cw <= sw) {
-                    result = ((cw - gw) / (sw - gw)) * 100;
-                } else
-                    result = 1;
+
+
+        cw = Float.parseFloat(c); // 현재
+        sw = Float.parseFloat(s); // 시작
+        gw = Float.parseFloat(g); // 목표
+
+        if (sw < gw) { // 시작이 작을때
+            if (cw <= gw && cw >= sw) {
+                result = ((cw - sw) / (gw - sw)) * 100;
+            } else
+                result = 1; //범위 밖에 있을 때
+        } else if (sw > gw) { // 시작이 더 클때
+            if (cw >= gw && cw <= sw) {
+                result = ((sw - cw) / (sw - gw)) * 100;
             }
+            else
+                result = 100;
         }
+        else {
+            result = 100;
+        }
+
         return result;
     }
 
@@ -155,6 +157,34 @@ public class Customercontent1 extends Fragment {
     protected void showList() {
         try {
             JSONObject jsonObj = new JSONObject(myJSON);
+
+            peoples = jsonObj.getJSONArray("data");
+
+            for (int i = 0; i < peoples.length(); i++) {
+                JSONObject c = peoples.getJSONObject(i);
+
+
+                //TODO 여기부분을 원하는 값으로 넣고 프로그레스바 움직이면됌
+                cc1txt1.setText(c.getDouble("weight") + "");
+                cc1txt2.setText(c.getDouble("goalweight") + ""); // 목표몸무게);
+                crtWeight = c.getDouble("presentweight") + "";// 현제 몸무게
+                cc1pBar.setProgress((int) goalRate(crtWeight, c.getDouble("weight") + "", c.getDouble("goalweight") + ""));
+
+                cc1Dialog.setStartWeight(c.getDouble("weight") + "");
+                cc1Dialog.setGoalWeight(c.getDouble("goalweight") + "");
+                cc1Dialog.setCurrentWeight(c.getDouble("presentweight") + "");
+            }
+
+            peoples = jsonObj.getJSONArray("info");
+
+            for (int i = 0; i < peoples.length(); i++) {
+                JSONObject c = peoples.getJSONObject(i);
+                if (c.getString("trainr").equals("")) {
+                    oneword.setText("아직 입력사항이 없습니다.");
+                } else {
+                    oneword.setText(c.getString("trainr"));
+                }
+            }
             peoples = jsonObj.getJSONArray("exercise");
 
             for (int i = 0; i < peoples.length(); i++) {
@@ -164,34 +194,9 @@ public class Customercontent1 extends Fragment {
                 String content = c.getString("content");
                 int set = c.getInt("ECset");
                 int number = c.getInt("ECnumber");
-                cc1list.add(new MyCustomDTO2(false,name, content));
+                cc1list.add(new MyCustomDTO2(false, name, content));
                 cc1list.get(i).setexercise(set);
                 cc1list.get(i).setnumber(number);
-            }
-            cc1adapter.notifyDataSetChanged();
-            peoples = jsonObj.getJSONArray("data");
-
-            for (int i = 0; i < peoples.length(); i++) {
-                JSONObject c = peoples.getJSONObject(i);
-
-
-                //TODO 여기부분을 원하는 값으로 넣고 프로그레스바 움직이면됌
-                double weight = c.getDouble("weight");  // 초기 몸무게
-                double goal = c.getDouble("goalweight"); // 목표몸무게
-                double pressent = c.getDouble("presentweight");// 현제 몸무게
-
-
-            }
-            peoples = jsonObj.getJSONArray("info");
-
-            for (int i = 0; i < peoples.length(); i++) {
-                JSONObject c = peoples.getJSONObject(i);
-                if(c.getString("trainr").equals("")){
-                    oneword.setText("아직 입력사항이 없습니다.");
-                }
-                else {
-                    oneword.setText(c.getString("trainr"));
-                }
             }
             cc1adapter.notifyDataSetChanged();
         } catch (JSONException e) {
@@ -214,7 +219,7 @@ public class Customercontent1 extends Fragment {
                 try {
                     HttpClient httpclient = new DefaultHttpClient();
                     HttpPost httppost = new HttpPost(uri);
-                    httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs,"UTF-8"));
+                    httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs, "UTF-8"));
                     HttpResponse response = httpclient.execute(httppost);
                     HttpEntity entity = response.getEntity();
                     is = entity.getContent();
@@ -254,6 +259,6 @@ public class Customercontent1 extends Fragment {
 
 
         GetDataJSON g = new GetDataJSON();
-        g.execute(url,id);
+        g.execute(url, id);
     }
 }
